bf84a72d54fbd8e5e5d0ea332273db98724ce568
fuongcao <phuongcaodev@gmail.com>
2024-06-28 14:28:11 +0700

    update ui

---
+++ app/privacy/page.tsx
@@ -0,0 +1,80 @@
+export default function page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Finds Now Privacy Policy &nbsp;
+        </p>
+      </div>
+
+      <div className="">
+        <h1> Privacy Policy for FINDS NOW</h1>
+        <p>
+          {" "}
+          If you require any more information or have any questions about our
+          privacy policy, please feel free to contact us by email at
+          [CONTACT@YOUREMAIL.COM].
+        </p>
+        <p>
+          At [YOUR SITE URL] we consider the privacy of our visitors to be
+          extremely important. This privacy policy document describes in detail
+          the types of personal information is collected and recorded by [YOUR
+          SITE URL] and how we use it.{" "}
+        </p>
+        <p>
+          {" "}
+          <b>Log Files</b>
+          <br /> Like many other Web sites, [YOUR SITE URL] makes use of log
+          files. These files merely logs visitors to the site – usually a
+          standard procedure for hosting companies and a part of hosting
+          services's analytics. The information inside the log files includes
+          internet protocol (IP) addresses, browser type, Internet Service
+          Provider (ISP), date/time stamp, referring/exit pages, and possibly
+          the number of clicks. This information is used to analyze trends,
+          administer the site, track user's movement around the site, and gather
+          demographic information. IP addresses, and other such information are
+          not linked to any information that is personally identifiable.{" "}
+        </p>
+        <p>
+          {" "}
+          <b>Cookies and Web Beacons</b>
+          <br />
+          [YOUR SITE URL] uses cookies to store information about visitors'
+          preferences, to record user-specific information on which pages the
+          site visitor accesses or visits, and to personalize or customize our
+          web page content based upon visitors' browser type or other
+          information that the visitor sends via their browser.{" "}
+        </p>
+        <p>
+          <b>DoubleClick DART Cookie</b>
+          <br />{" "}
+        </p>
+        <p>
+          → Google, as a third party vendor, uses cookies to serve ads on [YOUR
+          SITE URL].
+          <br />
+          <br />
+          → Google's use of the DART cookie enables it to serve ads to our
+          site's visitors based upon their visit to [YOUR SITE URL] and other
+          sites on the Internet. <br />
+          <br />→ Users may opt out of the use of the DART cookie by visiting
+          the Google ad and content network privacy policy at the following URL
+          –{" "}
+          <a href="http://www.google.com/privacy_ads.html">
+            http://www.google.com/privacy_ads.html
+          </a>{" "}
+        </p>
+        <p>
+          <b>Our Advertising Partners</b>
+          <br />{" "}
+        </p>
+        <p>
+          {" "}
+          Some of our advertising partners may use cookies and web beacons on
+          our site. Our advertising partners include ……. <br />
+        </p>
+      </div>
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left"></div>
+    </main>
+  );
+}
