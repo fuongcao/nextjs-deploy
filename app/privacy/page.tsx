85d23264ccf27580aac52a39ef1822ec60cbcacf
fuongcao <phuongcaodev@gmail.com>
2024-06-28 14:32:26 +0700

    fix eslint

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -27,23 +27,24 @@ export default function page() {
           <br /> Like many other Web sites, [YOUR SITE URL] makes use of log
           files. These files merely logs visitors to the site – usually a
           standard procedure for hosting companies and a part of hosting
-          services's analytics. The information inside the log files includes
-          internet protocol (IP) addresses, browser type, Internet Service
-          Provider (ISP), date/time stamp, referring/exit pages, and possibly
-          the number of clicks. This information is used to analyze trends,
-          administer the site, track user's movement around the site, and gather
-          demographic information. IP addresses, and other such information are
-          not linked to any information that is personally identifiable.{" "}
+          services`&apos;`s analytics. The information inside the log files
+          includes internet protocol (IP) addresses, browser type, Internet
+          Service Provider (ISP), date/time stamp, referring/exit pages, and
+          possibly the number of clicks. This information is used to analyze
+          trends, administer the site, track user`&apos;`s movement around the
+          site, and gather demographic information. IP addresses, and other such
+          information are not linked to any information that is personally
+          identifiable.{" "}
         </p>
         <p>
           {" "}
           <b>Cookies and Web Beacons</b>
           <br />
-          [YOUR SITE URL] uses cookies to store information about visitors'
-          preferences, to record user-specific information on which pages the
-          site visitor accesses or visits, and to personalize or customize our
-          web page content based upon visitors' browser type or other
-          information that the visitor sends via their browser.{" "}
+          [YOUR SITE URL] uses cookies to store information about
+          visitors`&apos;` preferences, to record user-specific information on
+          which pages the site visitor accesses or visits, and to personalize or
+          customize our web page content based upon visitors`&apos;` browser
+          type or other information that the visitor sends via their browser.{" "}
         </p>
         <p>
           <b>DoubleClick DART Cookie</b>
@@ -54,9 +55,9 @@ export default function page() {
           SITE URL].
           <br />
           <br />
-          → Google's use of the DART cookie enables it to serve ads to our
-          site's visitors based upon their visit to [YOUR SITE URL] and other
-          sites on the Internet. <br />
+          → Google`&apos;`s use of the DART cookie enables it to serve ads to
+          our site`&apos;`s visitors based upon their visit to [YOUR SITE URL]
+          and other sites on the Internet. <br />
           <br />→ Users may opt out of the use of the DART cookie by visiting
           the Google ad and content network privacy policy at the following URL
           –{" "}
